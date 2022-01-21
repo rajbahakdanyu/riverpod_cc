7f0e0425b2ee55ed998a0b0ed470858191f49ed6
danyu rajbahak <rajbahakdanyu@gmail.com>
2022-01-21 22:17:34 +0545

    Added tab bar

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomeScreen extends ConsumerWidget {
   HomeScreen({Key? key}) : super(key: key);
@@ -11,30 +12,51 @@ class HomeScreen extends ConsumerWidget {
     return Scaffold(
       body: SizedBox(
         width: MediaQuery.of(context).size.width,
-        child: Column(
-          children: [
-            Text(
-              'Todo',
-              style: Theme.of(context).textTheme.headline3,
-            ),
-            TextFormField(
-              decoration: InputDecoration(
-                hintText: 'Enter task here',
-                constraints: BoxConstraints(
-                  maxWidth: MediaQuery.of(context).size.width * .8,
-                ),
-                border: const OutlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Colors.purpleAccent,
+        child: DefaultTabController(
+          length: 4,
+          child: Column(
+            children: [
+              Text(
+                'Todo',
+                style: Theme.of(context).textTheme.headline3,
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                  hintText: 'Enter task here',
+                  constraints: BoxConstraints(
+                    maxWidth: MediaQuery.of(context).size.width * .8,
+                  ),
+                  border: const OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.purpleAccent,
+                    ),
                   ),
                 ),
+                controller: taskController,
+                keyboardType: TextInputType.text,
+                textInputAction: TextInputAction.done,
+                maxLines: 1,
+                onFieldSubmitted: (value) {},
               ),
-              controller: taskController,
-              keyboardType: TextInputType.text,
-              textInputAction: TextInputAction.done,
-              maxLines: 1,
-            ),
-          ],
+              const SizedBox(height: 20),
+              SizedBox(
+                width: MediaQuery.of(context).size.width * .8,
+                child: TabBar(
+                  labelColor: Colors.deepPurple,
+                  labelStyle: Theme.of(context).textTheme.bodyText2!.copyWith(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                  tabs: const [
+                    Text('All'),
+                    Text('Doing'),
+                    Text('Done'),
+                    Text('Favorite'),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
