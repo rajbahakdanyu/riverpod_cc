fd46ad3d3ef3ce73f571eca2078ba2ad30e5961f
danyu rajbahak <rajbahakdanyu@gmail.com>
2022-01-21 21:43:46 +0545

    updated home screen

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final counterProvider = StateProvider((ref) => 0);
-
 class HomeScreen extends ConsumerWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
@@ -13,34 +11,9 @@ class HomeScreen extends ConsumerWidget {
         elevation: 0,
         title: const Text('Todo'),
       ),
-      body: Center(
-        child: Consumer(
-          builder: (context, ref, _) {
-            final count = ref.watch(counterProvider.state).state;
-            return Text('$count');
-          },
-        ),
-      ),
-      floatingActionButton: Row(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          FloatingActionButton(
-            heroTag: null,
-            onPressed: () => ref.read(counterProvider.state).state--,
-            child: const Icon(Icons.remove),
-          ),
-          const SizedBox(width: 10),
-          FloatingActionButton(
-            heroTag: null,
-            onPressed: () => ref.read(counterProvider.state).state = 0,
-            child: const Icon(Icons.close),
-          ),
-          const SizedBox(width: 10),
-          FloatingActionButton(
-            heroTag: null,
-            onPressed: () => ref.read(counterProvider.state).state++,
-            child: const Icon(Icons.add),
-          ),
+      body: Column(
+        children: const [
+          Text('Todo'),
         ],
       ),
     );
