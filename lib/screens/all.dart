b84ca5e3a88c36437abe9d386cde1d64a4761e9b
danyu rajbahak <rajbahakdanyu@gmail.com>
2022-01-21 22:24:06 +0545

    Added all screen

---
+++ lib/screens/all.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class AllScreen extends ConsumerStatefulWidget {
+  const AllScreen({Key? key}) : super(key: key);
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _AllScreenState();
+}
+
+class _AllScreenState extends ConsumerState<AllScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 class HomeScreen extends ConsumerWidget {
   HomeScreen({Key? key}) : super(key: key);
